d9fa5a7e8dfe7df6363f539d78cc7f7b68dd5278
chaejun12 <chaejun12@gmail.com>
2026-05-22 21:31:26 +0900

    feat: Add GitHub Pages deploy workflow and configure Vite base url

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/BINGLE_MVP/',
   plugins: [react()],
   server: {
     port: 3000,
     open: true
   }
 });
+
